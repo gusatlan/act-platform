179bac57aed3473ffcbdeebfc7d2926d97ff2feb
gustavooliveira <gusatlan@gmail.com>
2023-05-11 13:31:27 -0300

    Modificado script compile.bat. Só executando o docker build

--- compile.bat
+++ compile.bat
@@ -1,9 +1,10 @@
 @ECHO OFF
 
-gradlew clean test build publish
 docker build -t act-platform-image:1.0.0 .
 docker build -t act-platform-image:latest .
 
-#docker run --rm -it act-platform-image:latest bash
+REM gradlew clean test build publish
+
+REM docker run --rm -it act-platform-image:latest bash
 
 ECHO ON
